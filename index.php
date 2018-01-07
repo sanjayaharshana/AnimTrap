bea59c55decad2ef82104ba43bbd00b48694d01d
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2018-01-08 01:39:51 +0530

    Basic Files and Testing Sample php File
    i created a basic css and js file

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> SourceWallet Dev </title>
+    <meta name"Software Development Solutions" "Sri Lanken Best Software/Web Development Solutions">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/animtrap.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="anim-hover-bob">
+        <h2>hjhkjhkds</h2>
+    </div>
+
+    <div class="snow">
+        kfdjlfkjd
+    </div>
+</body>
+
+<script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+
+
+
+<script  src="js/index.js"></script>
+
+
+
+
+</body>
+
+</html>
+
+
+<script>
+    ANIMSCROLL.init({
+        easing: 'ease-out-back',
+        duration: 1000
+    });
+</script>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/anim-scroll.js"></script>
