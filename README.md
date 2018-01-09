420eb2a6b7494f0d09377ceb70287a3fac820e3a
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2018-01-09 23:36:56 +0530

    updating weather effect
    updating rain and lightning animation classs

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # AnimTrap
+
+<h4>Get Started</h4>
+
+<p>AnimTrap (currently v1.1.1) has a few easy ways to quickly get started, each one appealing to a different skill level and use case. Read through to see what suits your particular needs.</p>
+
+<h4>Open Source Contributing</h4>
+
+<p>We are always looking for the quality contributions and will be happy to accept your Pull Requests as long as those adhere to some basic rules:</p>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <title> SourceWallet Dev </title>
     <meta name"Software Development Solutions" "Sri Lanken Best Software/Web Development Solutions">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/animtrap.css" rel="stylesheet">
+    <link href="css/animtrap-bin.css" rel="stylesheet">
 </head>
 
 <body>
@@ -16,6 +16,9 @@
         kfdjlfkjd
     </div>
 </body>
+
+<div class="anim-lightning-normal" style="background: url('o.jpg');"></div>
+<div class="anim-lightning-flash doing" style="background: url('o.jpg');"></div>
 
 <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
 
