322e0af97ce8e7413814e860a8808cd542e17a4a
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2018-01-10 05:38:50 +0530

    making sample folder
    making sample folder

--- Demos/index.php
+++ Demos/index.php
@@ -1,24 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title> SourceWallet Dev </title>
+    <title>AnimTrap Lightning</title>
     <meta name"Software Development Solutions" "Sri Lanken Best Software/Web Development Solutions">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/animtrap-bin.css" rel="stylesheet">
+    <link href="../css/animtrap.css" rel="stylesheet">
 </head>
 
 <body>
-    <div class="anim-hover-bob">
-        <h2>hjhkjhkds</h2>
-    </div>
-
-    <div class="snow">
-        kfdjlfkjd
-    </div>
+<div class="anim-lightning-normal" style="background: url('o.jpg'); background-size:cover;  "></div>
+<div class="anim-lightning-flash doing" style="background: url('o.jpg');background-size:cover;  "></div>
+<div class="rain front-row"></div>
+<h2 style="color: white">Welcome</h2>
 </body>
 
-<div class="anim-lightning-normal" style="background: url('o.jpg');"></div>
-<div class="anim-lightning-flash doing" style="background: url('o.jpg');"></div>
+
 
 <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
 
@@ -41,5 +37,5 @@
     });
 </script>
 
-<script src="js/jquery.min.js"></script>
-<script src="js/anim-scroll.js"></script>
+<script src="../js/jquery.min.js"></script>
+<script src="../js/anim-scroll.js"></script>
