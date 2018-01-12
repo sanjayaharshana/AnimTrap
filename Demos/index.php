d4fb75676f89eb29af49dbb4672ef924fd92f16b
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2018-01-12 09:14:55 +0530

    demo mod
    demo modifley

--- Demos/index.php
+++ Demos/index.php
@@ -5,37 +5,80 @@
     <meta name"Software Development Solutions" "Sri Lanken Best Software/Web Development Solutions">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../css/animtrap.css" rel="stylesheet">
+    <link href="extranal_css/bootstrap.css" rel="stylesheet">
 </head>
 
 <body>
 <div class="anim-lightning-normal" style="background: url('o.jpg'); background-size:cover;  "></div>
 <div class="anim-lightning-flash doing" style="background: url('o.jpg');background-size:cover;  "></div>
 <div class="rain front-row"></div>
-<h2 style="color: white">Welcome</h2>
-</body>
+
+<!-- Navbar Layer-->
+<nav class="navbar navbar-inverse navbar-static-top marginBottom-0" style="background: url('mwe.png'); background-size: auto">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+
+    </div>
+
+    <div class="collapse navbar-collapse text-center" id="navbar-collapse-1">
+        <ul class="nav navbar-nav">
+            <li class=""><a href="#">AnimTrap Download</a></li>
+            <li><a href="#1">Contrubtion</a></li>
+            <li><a href="#2">Demo Samples</a></li>
+            <li><a href="#3">Documentation </a></li>
+            <li><a href="#4">Suppoted for </a></li>
+            <li><a href="#9">About Us</a></li>
+        </ul>
+    </div>
+</nav>
 
 
+<div class="row">
+    <div class="container">
+        <div class="col-sm-4">
+            <div class="anim-hover-float">
+              <h4>What is the AnimTrap</h4><br>
+              <p>SourceWallet Dev is a boutique outsourcing company offering a full range of custom software development services for a wide variety of verticals and business domains.Whether you are a start-up or an established business, we will be happy to assist you at any and every stage of the software development life cycle: from conceptualization, business analysis and prototyping to the development and deployment of a complete solution.</p>
+             </div>
+            <a class="btn btn-primary" href="" role="button">Learn More</a>
+         </div>
+        <div class="col-sm-4">
+            <div class="anim-hover-float">
+                <h4>Best Animation Demo</h4><br>
+                <p>SourceWallet Dev is a boutique outsourcing company offering a full range of custom software development services for a wide variety of verticals and business domains.Whether you are a start-up or an established business, we will be happy to assist you at any and every stage of the software development life cycle: from conceptualization, business analysis and prototyping to the development and deployment of a complete solution.</p>
+            </div>
+            <a class="btn btn-primary" href="" role="button">Learn More</a>
+        </div>
 
-<script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
-
-
-
-<script  src="js/index.js"></script>
-
-
-
+        <div class="col-sm-4">
+            <div class="anim-hover-float">
+                <h4>Showcase</h4><br>
+                <p>SourceWallet Dev is a boutique outsourcing company offering a full range of custom software development services for a wide variety of verticals and business domains.Whether you are a start-up or an established business, we will be happy to assist you at any and every stage of the software development life cycle: from conceptualization, business analysis and prototyping to the development and deployment of a complete solution.</p>
+            </div>
+        </div>
+</div>
 
 </body>
 
 </html>
 
 
+
+
+
 <script>
-    ANIMSCROLL.init({
-        easing: 'ease-out-back',
-        duration: 1000
+    AOS.init({
+        offset: 200,
+        duration: 600,
+        easing: 'ease-in-sine',
+        delay: 100,
     });
 </script>
 
-<script src="../js/jquery.min.js"></script>
-<script src="../js/anim-scroll.js"></script>
+<script src="extranal_css/jquery.min.js"></script>
+<script src="extranal_css/anim-scroll.js"></script>
