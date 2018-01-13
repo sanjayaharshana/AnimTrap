75afa863f5dd26676fb774a590e713a396f0a292
Chamuth Chamandana <c.chamandana@gmail.com>
2018-01-13 12:02:01 +0530

    Removed self-dependency and added CDNs for demos/index

--- Demos/index.php
+++ Demos/index.php
@@ -1,86 +1,88 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	
     <title>AnimTrap Lightning</title>
     <meta name"Software Development Solutions" "Sri Lanken Best Software/Web Development Solutions">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="extranal_css/animtrap.min.css" rel="stylesheet">
-    <link href="extranal_css/bootstrap.css" rel="stylesheet">
+
+
+    <link href="https://cdn.rawgit.com/sanjayaharshana/AnimTrap/59cd6311/dist/css/animtrap.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-<div class="anim-lightning-normal" style="background: url('o2.jpg'); background-size:cover;  "></div>
-<div class="anim-lightning-flash doing" style="background: url('o2.jpg');background-size:cover;  "></div>
-<div class="rain front-row"></div>
 
-<!-- Navbar Layer-->
-<nav class="navbar navbar-inverse navbar-static-top marginBottom-0" style="background: url('mwe.png'); background-size: auto">
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
+	<div class="anim-lightning-normal" style="background: url('o2.jpg'); background-size:cover;  "></div>
+	<div class="anim-lightning-flash doing" style="background: url('o2.jpg');background-size:cover;  "></div>
+	<div class="rain front-row"></div>
 
-    </div>
+	<!-- Navbar Layer-->
+	<nav class="navbar navbar-inverse navbar-static-top marginBottom-0" style="background: url('mwe.png'); background-size: auto">
+	    <div class="navbar-header">
+	        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	        </button>
 
-    <div class="collapse navbar-collapse text-center" id="navbar-collapse-1">
-        <ul class="nav navbar-nav">
-            <li class=""><a href="#">AnimTrap Download</a></li>
-            <li><a href="#1">Contrubtion</a></li>
-            <li><a href="#2">Demo Samples</a></li>
-            <li><a href="#3">Documentation </a></li>
-            <li><a href="#4">Suppoted for </a></li>
-            <li><a href="#9">About Us</a></li>
-        </ul>
-    </div>
-</nav>
+	    </div>
+
+	    <div class="collapse navbar-collapse text-center" id="navbar-collapse-1">
+	        <ul class="nav navbar-nav">
+	            <li class=""><a href="#">AnimTrap Download</a></li>
+	            <li><a href="#1">Contrubtion</a></li>
+	            <li><a href="#2">Demo Samples</a></li>
+	            <li><a href="#3">Documentation </a></li>
+	            <li><a href="#4">Suppoted for </a></li>
+	            <li><a href="#9">About Us</a></li>
+	        </ul>
+	    </div>
+	</nav>
 
 
-<div class="row">
-    <div class="container">
-        <div class="col-sm-4">
-            <div class="anim-hover-float">
-                <h4>What is the AnimTrap</h4><br>
-                <p>SourceWallet Dev is a boutique outsourcing company offering a full range of custom software development services for a wide variety of verticals and business domains.Whether you are a start-up or an established business, we will be happy to assist you at any and every stage of the software development life cycle: from conceptualization, business analysis and prototyping to the development and deployment of a complete solution.</p>
-            </div>
-            <a class="btn btn-primary" href="" role="button">Learn More</a>
-        </div>
-        <div class="col-sm-4">
-            <div class="anim-hover-float">
-                <h4>Best Animation Demo</h4><br>
-                <p>SourceWallet Dev is a boutique outsourcing company offering a full range of custom software development services for a wide variety of verticals and business domains.Whether you are a start-up or an established business, we will be happy to assist you at any and every stage of the software development life cycle: from conceptualization, business analysis and prototyping to the development and deployment of a complete solution.</p>
-            </div>
-            <a class="btn btn-primary" href="" role="button">Learn More</a>
-        </div>
+	<div class="row">
+	    <div class="container">
+	        <div class="col-sm-4">
+	            <div class="anim-hover-float">
+	                <h4>What is the AnimTrap</h4><br>
+	                <p>SourceWallet Dev is a boutique outsourcing company offering a full range of custom software development services for a wide variety of verticals and business domains.Whether you are a start-up or an established business, we will be happy to assist you at any and every stage of the software development life cycle: from conceptualization, business analysis and prototyping to the development and deployment of a complete solution.</p>
+	            </div>
+	            <a class="btn btn-primary" href="" role="button">Learn More</a>
+	        </div>
+	        <div class="col-sm-4">
+	            <div class="anim-hover-float">
+	                <h4>Best Animation Demo</h4><br>
+	                <p>SourceWallet Dev is a boutique outsourcing company offering a full range of custom software development services for a wide variety of verticals and business domains.Whether you are a start-up or an established business, we will be happy to assist you at any and every stage of the software development life cycle: from conceptualization, business analysis and prototyping to the development and deployment of a complete solution.</p>
+	            </div>
+	            <a class="btn btn-primary" href="" role="button">Learn More</a>
+	        </div>
 
-        <div class="col-sm-4">
-            <div class="anim-hover-float">
-                <h4>Showcase</h4><br>
-                <p>SourceWallet Dev is a boutique outsourcing company offering a full range of custom software development services for a wide variety of verticals and business domains.Whether you are a start-up or an established business, we will be happy to assist you at any and every stage of the software development life cycle: from conceptualization, business analysis and prototyping to the development and deployment of a complete solution.</p>
-            </div>
-        </div>
-    </div>
+	        <div class="col-sm-4">
+	            <div class="anim-hover-float">
+	                <h4>Showcase</h4><br>
+	                <p>SourceWallet Dev is a boutique outsourcing company offering a full range of custom software development services for a wide variety of verticals and business domains.Whether you are a start-up or an established business, we will be happy to assist you at any and every stage of the software development life cycle: from conceptualization, business analysis and prototyping to the development and deployment of a complete solution.</p>
+	            </div>
+	        </div>
+	    </div>
+	</div>
+
+	<script type="text/javascript">
+	    ANIMSCROLL.init({
+	        offset: 200,
+	        duration: 600,
+	        easing: 'ease-in-sine',
+	        delay: 100,
+	    });
+	</script>
+
+
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="https://cdn.rawgit.com/sanjayaharshana/AnimTrap/59cd6311/dist/js/anim-scroll.min.js"></script>
 
 </body>
 
 </html>
-
-
-
-
-
-<script>
-    ANIMSCROLL.init({
-        offset: 200,
-        duration: 600,
-        easing: 'ease-in-sine',
-        delay: 100,
-    });
-</script>
-
-
-
-<script src="extranal_css/jquery.min.js"></script>
-<script src="extranal_css/anim-scroll.js"></script>
