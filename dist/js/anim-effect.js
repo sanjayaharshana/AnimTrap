ca1e4bf54883c996473b5791ab27cbae7ef0cdd4
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2018-02-25 13:34:09 +0530

    Merge branch 'master' of https://github.com/sanjayaharshana/AnimTrap
                       # Conflicts:
                       #	Demos/extranal_css/anim-scroll.js
                       #	Demos/index.php

--- dist/js/anim-effect.js
+++ dist/js/anim-effect.js
@@ -1,7 +1,6 @@
 (function ( $ ) {
 
-    
-
+    $.fn.makeitsRain = function( options ) {
         // This is the easiest way to have default options.
         var settings = $.extend({
             // These are the defaults.
